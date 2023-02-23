ce2b2b0e66eb470b02d4e410bb3b9b1279686dde
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-02-23 15:18:42 -0800

    fix: restore createWindow function (#8)

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-declare module "domino" {
+declare module 'domino' {
   function createDOMImplementation(): DOMImplementation;
   function createDocument(html?: string, force?: boolean): Document;
   function createWindow(html?: string, address?: string): Window;
